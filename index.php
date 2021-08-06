0cef2b1566fa40adb7ba18f4abe30a5b1b3a4f85
URIAN VIERA <urian1213viera@gmail.com>
2021-08-05 22:33:26 -0500

    cambios finales

--- index.php
+++ index.php
@@ -21,24 +21,30 @@
 <div class="container mt-5"> 
   
   <h3 class="text-center mb-5" style="font-weight: 800; font-size: 35px">
-    Cómo (Sumar - Restar) Dia, Mes y Año a una fecha con PHP <strong>Archivo</strong> 
+    Cómo (Sumar - Restar) Dia, Mes y Año a una fecha con PHP
     <hr>
   </h3>
 
 <div class="row text-center">
     <div class="col-md-6">
       <p  id="titleP">
-        Agregar Dia, Mes y Año
+        Sumar Dia, Mes y Año
       </p>
     <?php 
-      $fechaDiaUno = date("Y-m-d");
-      echo '<p id="dia">Agregando Dia: '. date('Y-m-d', strtotime($fechaDiaUno. ' + 1 days')).'</p>';
+    date_default_timezone_set("America/Bogota");
+    setlocale(LC_ALL,"es_ES");
+    setlocale(LC_TIME, 'es_ES.UTF-8');
+    //La función de PHP strtotime(): 
+    //Consiste en convierte una cadena con formato de fecha/hora en otra con formato de fecha Unix.
+
+      $fechaDiaUno = date("Y-m-d"); //2021-08-05
+      echo '<p id="dia">Agregando Dia: '. date('Y-m-d', strtotime($fechaDiaUno. ' + 2 days')).'</p>';
 
       $fechaMesDos = date("Y-m-d");
-      echo '<p id="mes">Agregando Dia: '. date('Y-m-d', strtotime($fechaMesDos. ' + 1 month')).'</p>';
+      echo '<p id="mes">Agregando Mes: '. date('Y-m-d', strtotime($fechaMesDos. ' + 2 month')).'</p>';
 
       $fechaYearDos = date("Y-m-d");
-      echo '<p id="year">Agregando Dia: '. date('Y-m-d', strtotime($fechaYearDos. ' + 1 year')).'</p>';
+      echo '<p id="year">Agregando Año: '. date('Y-m-d', strtotime($fechaYearDos. ' + 2 year')).'</p>';
     ?>
   </div>
 
@@ -51,18 +57,24 @@
 
   <?php 
     $fechaDiaRestar = date("Y-m-d");
-    echo '<p id="diaUno">Restando Dia: '. date('Y-m-d', strtotime($fechaYearDos. ' - 1 days')).'</p>';
+    echo '<p id="diaUno">Restando Dia: '. date('Y-m-d', strtotime($fechaYearDos. ' - 2 days')).'</p>';
 
     $fechaMesRestar = date("Y-m-d");
-    echo '<p id="mesDos">Restando Mes: '. date('Y-m-d', strtotime($fechaMesRestar. ' - 1 month')).'</p>';
+    echo '<p id="mesDos">Restando Mes: '. date('Y-m-d', strtotime($fechaMesRestar. ' - 2 month')).'</p>';
 
     $fechaYearRestar = date("Y-m-d");
-    echo '<p id="yearDos">Agregando Año: '. date('Y-m-d', strtotime($fechaYearRestar. ' - 1 year')).'</p>';
+    echo '<p id="yearDos">Agregando Año: '. date('Y-m-d', strtotime($fechaYearRestar. ' - 6 year')).'</p>';
   ?>
 
     </div>
   </div>
 
+
+<div class="row text-center">
+    <div class="col-md-12">
+      <img src="fecha.jpg" width="300px">
+    </div>
+</div>
 
 </div>
 
